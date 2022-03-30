b5822dbf17345f0a926d8ae33f959eb1f610023b
Welton Vaz de Souza <weltonvaz@gmail.com>
2022-03-30 10:03:36 -0300

    second commit

---
+++ scripts/welton.js
@@ -0,0 +1,28 @@
+/*
+Therefore, your task today is to rewrite the code below so that it prints the information correctly, 
+makes sense and without errors:
+*/
+let numeroUm = 1
+let stringUm = '1'
+let numeroTrinta = 30
+let stringTrinta = '30'
+let numeroDez = 10
+let stringDez = '10'
+
+if (numeroUm == parseInt(stringUm)) {
+  console.log('As variáveis numeroUm e stringUm tem o mesmo valor, mas tipos diferentes')
+} else {
+  console.log('As variáveis numeroUm e stringUm não tem o mesmo valor')
+}
+
+if (numeroTrinta == parseInt(stringTrinta)) {
+  console.log('As variáveis numeroTrinta e stringTrinta tem o mesmo valor e mesmo tipo')
+} else {
+  console.log('As variáveis numeroTrinta e stringTrinta não tem o mesmo tipo')
+}
+
+if (numeroDez == parseInt(stringDez)) {
+  console.log('As variáveis numeroDez e stringDez tem o mesmo valor, mas tipos diferentes')
+} else {
+  console.log('As variáveis numeroDez e stringDez não tem o mesmo valor')
+}
